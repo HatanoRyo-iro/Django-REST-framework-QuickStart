ed52b8a2b0e7da5ab6cb4a1227a2ad9cc134b8a8
RyoHatano <ryohatano0809@gmail.com>
2023-05-12 15:01:30 +0900

    first settings

---
+++ Django_REST_framework_tutorial/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for Django_REST_framework_tutorial project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "Django_REST_framework_tutorial.settings"
+)
+
+application = get_wsgi_application()
